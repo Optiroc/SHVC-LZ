72935efbeec2e2c0d3245dba293e20ba69168130
David Lindecrantz <optiroc@me.com>
2024-04-01 01:12:46 +0200

    Add LZSA2_DecompressBlock call to main example

--- src/main.s
+++ src/main.s
@@ -10,6 +10,29 @@
 .autoimport
 .export Main
 
+Destination = $7f0000
+
 Main:
-:   wai             ; Wait indefinitely
-    bra :-          ; (only wake up in NMI)
+    .a8
+    .i16
+
+    ; Set source/destination
+    ;
+    ; LZSA2_DecompressBlock requires the following parameters:
+    ;   x           Source offset
+    ;   y           Destination offset
+    ;   b:a         Destination:Source banks
+    ldy #.loword(Destination)
+    ldx #.loword(Compressed)
+    lda #^Destination
+    xba
+    lda #^Compressed
+
+    jsl LZSA2_DecompressBlock
+
+:   wai
+    bra :-
+
+.segment "RODATA"
+Compressed:
+    .incbin "../data/abam.txt.lzsa2"
